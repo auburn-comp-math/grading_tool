72a779d9dfe0f4d96bf83d74ea650339720091a5
Yimin Zhong <yzz0225@auburn.edu>
2024-09-06 17:26:33 -0500

    update

--- grader.py
+++ grader.py
@@ -23,14 +23,18 @@ class Grader():
         self.submission_dir = submission_dir
         self.test_dir = test_dir
 
+        self.matlab_test = []
+        self.python_test = []
+
 
         for file in os.listdir(test_dir):
             if file.endswith('.m'):
-                self.matlab_test = file
-                print(f"MATLAB test {self.matlab_test} found!\n")
+                self.matlab_test.append(file)
             elif file.endswith('.py'):
-                self.python_test = file
-                print(f"PYTHON test {self.python_test} found!\n")
+                self.python_test.append(file)
+
+        print(f"MATLAB test {self.matlab_test} found!\n")
+        print(f"PYTHON test {self.python_test} found!\n")
 
         print('==============  Grader initialized! =============\n\n')
 
@@ -57,6 +61,9 @@ class Grader():
     def matlab_grade(self, student_path, hw_str='hw00'):
         """
         Grade the student's MATLAB code
+
+        @param student_path: The student's directory
+        @param hw_str: The homework string
         """
         with open(os.path.join(student_path, hw_str + '.m'), 'r', encoding='utf-8') as code_file:
             # get author information
@@ -64,16 +71,22 @@ class Grader():
             if email.find('@')== -1:
                 email = NULL_EMAIL
             # copy the test file to the student's directory
-            shutil.copy(os.path.join(self.test_dir, self.matlab_test), student_path)
+            for _test in self.matlab_test:
+                shutil.copy(os.path.join(self.test_dir, _test), student_path)
             # run the test file
-            student_score = execute_system_call(\
-                    f'matlab -nojvm -nosplash -nodesktop -batch \
-                    "run(\'{os.path.join(student_path, self.matlab_test)}\');exit;"')
+            student_score = ""
+            for _test in self.matlab_test:
+                student_score += execute_system_call(\
+                        f'matlab -nojvm -nosplash -nodesktop -batch \
+                        "run(\'{os.path.join(student_path, _test)}\');exit;"')
             return student_score.count('PASS'), email
 
     def python_grade(self, student_path, hw_str='hw00'):
         """
         Grade the student's Python code
+
+        @param student_path: The student's directory
+        @param hw_str: The homework string
         """
         with open(os.path.join(student_path, hw_str + '.py'), 'r', encoding='utf-8') as code_file:
             # get author information
@@ -81,15 +94,21 @@ class Grader():
             if email.find('@')== -1:
                 email = NULL_EMAIL
             # copy the test file to the student's directory
-            shutil.copy(os.path.join(self.test_dir, self.python_test), student_path)
+            for _test in self.python_test:
+                shutil.copy(os.path.join(self.test_dir, _test), student_path)
             # run the test file
-            student_score = execute_system_call(\
-                    f'python \'{os.path.join(student_path, self.python_test)}\'')
+            student_score = ""
+            for _test in self.python_test:
+                student_score += execute_system_call(\
+                        f'python \"{os.path.join(student_path, _test)}\"')
             return student_score.count('PASS'), email
 
     def convert_to_python(self, student_path, hw_str='hw00'):
         """
         Convert the student's Jupyter notebook to Python script
+
+        @param student_path: The student's directory
+        @param hw_str: The homework string
         """
         execute_system_call(f'jupyter nbconvert --to python\
                              {os.path.join(student_path, hw_str + ".ipynb")}')
@@ -97,6 +116,9 @@ class Grader():
     def grade_exception_file(self, hw_str, student_dir):
         """
         Handle the file name exceptions
+
+        @param hw_str: The homework string
+        @param student_dir: The student directory
         """
         local_files = os.listdir(os.path.join(student_dir))
         for _file in local_files:
@@ -136,6 +158,9 @@ class Grader():
     def grade_standard_file(self, hw_str, student_dir):
         """
         Grade the standard file name
+
+        @param hw_str: The homework string
+        @param student_dir: The student directory
         """
         data = []
 
@@ -154,6 +179,10 @@ class Grader():
     def println(self, i, student_info, item):
         """
         Print the student's score
+
+        @param i: The student index
+        @param student_info: The student information
+        @param item: The student item
         """
         cnt_passes, email, student_code = item
         print(f'Student {(i+1): 3d}/{self.total_students: 3d}\
@@ -163,6 +192,11 @@ class Grader():
     def output(self, grades_file, i, student_info, data):
         """
         Output the grades to the file
+
+        @param grades_file: The grades file
+        @param i: The student index
+        @param student_info: The student information
+        @param data: The student data
         """
         for _item in data:
             cnt_passes, email, student_code = _item
@@ -175,6 +209,7 @@ class Grader():
         Grade the students
 
         @param hw_str: The homework string
+        @param output_file: The output file
         """
         # create a file to store the grades
         with open(output_file, 'w', encoding='utf-8') as grades_file:
@@ -183,6 +218,8 @@ class Grader():
             if not os.path.exists(self.submission_dir):
                 print('Unzipping the submission file ...')
                 unzip(self.submission_file, self.submission_dir, skip_dir=False)
+
+            print(f'Submissions unzipped in {self.submission_dir}\n')
 
             student_dirs = self.get_dirs()
 
